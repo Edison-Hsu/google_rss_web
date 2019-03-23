a2ca63b786102cc69e4b893ca493a498b3da0b1e
xudongdong <dongdong.xu1@thoughtworks.com>
2019-03-23 21:12:57 +0800

    add root path

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/search', to: 'search#index'
   post '/search', to: 'search#create'
+
+  root 'search#index'
 end
